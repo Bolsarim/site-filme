012242692f776d6a27d811060b05e922e663005b
Bolsarim <giovanna.bolsarim@gmail.com>
2024-07-09 20:14:20 -0300

    comeco

---
+++ app.js
@@ -0,0 +1,11 @@
+function Menu(){
+    var menu = document.getElementById("menuvert");
+
+
+    if(menu.style.display ==="none"){
+        menu.style.display="block";
+
+    }else{
+        menu.style.display="none";
+    }
+}
